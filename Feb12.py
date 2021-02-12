6ec7173b3c259657162f0815eb8acece7bef231c
woooooooOT <78742794+woooooooOT@users.noreply.github.com>
2021-02-12 09:52:04 -0500

    feb12_1

--- Feb12.py
+++ Feb12.py
@@ -1,1 +1,11 @@
+import random
+
 print('My favourite book is \"Magic Treehouse\"\nand \nMy favourite character is this: \"\\\" the backward slash ')
+
+print (random.randint(1,999))
+
+for x in range(0,10):
+    print (x)#for(int n, n<10, n++){}
+
+x = int(2.5e4)
+print (x)
